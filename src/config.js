e7eadf757052e14517c118c9e24b74647ec36ad2
xzh <793414851@qq.com>
2017-05-06 09:00:15 +0800

    修改config.js的路径解决bug

--- src/config.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-	paths:{
-		"jquery":"lib/jquery-3.1.1",
-		"jqueryUi":"lib/jquery-ui-1.12.1.custom/jquery-ui"
-	},
-	shim:{
-		"jqueryUi":["jquery"]
-	}
-});
---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+requirejs.config({
+	paths:{
+		"jquery":"../lib/jquery-3.1.1",
+		"jqueryUi":"../lib/jquery-ui-1.12.1.custom/jquery-ui"
+	},
+	shim:{
+		"jqueryUi":["jquery"]
+	}
+});
