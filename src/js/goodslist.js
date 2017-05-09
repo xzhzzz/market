3fc4c025b11a314494978e2a08037b0737f02d9c
xzh <793414851@qq.com>
2017-05-09 16:46:37 +0800

    终极修改列表页预加载

--- src/js/goodslist.js
+++ src/js/goodslist.js
@@ -63,6 +63,7 @@ requirejs(['config'],function(){
 					};
 				};
 			};
+			
 			//获取长度
 			$.ajax({
 				type:"get",
@@ -71,6 +72,48 @@ requirejs(['config'],function(){
 				dataType:"json",
 				success:function(resp){
 					pageLen=Math.ceil(resp.total/qty);
+					$.ajax({
+							type:"get",
+							url:"../php/getList.php",
+							data:{"pageNo":pageNo,"qty":4},
+							dataType:"json",
+							success:function(resp){
+								flag=true;
+								var arr=resp.data;//获取后端返回的数据中要转换成标签的数组
+								//根据返回的数组长度生成section盒子
+								for(var i=0;i<arr.length;i++){
+									var res = `<div class="section">
+													<div class="section-t">
+														<span class="list-logo"></span>
+														<span clsaa="title">${arr[i].title}</span>
+													</div>
+													<div class="section-bot">
+														<ul class="ul${ul}">
+														</ul>
+													</div>
+												</div>`;
+									//插入到页面的位置
+									$('#list-content').append(res);
+									//获取数据中的li数组生成对象的li标签
+									var list=arr[i].li;
+									var li;
+									//根据li数组的长度生成对应数量的里标签
+									for(var j=0;j<list.length;j++){
+										//原生js创建li标签
+										li = document.createElement('li');
+										//把li数组中的a标签数组生成相对应的a标签然后插入li中
+										li.innerHTML=list[j].map(function(item){
+											return `<a href="">${item}</a>`;
+										}).join('');	
+										//li标签插入到生成的ul类中,防止类名的相同
+										$('.section-bot .ul'+ul).append(li);
+									};
+									//ul一直叠加防止类名重复导致数据插入的地方错误
+									ul++;
+								};
+								pageNo++;
+							}
+						});
 				},
 				async:true
 			});
