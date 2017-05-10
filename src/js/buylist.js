c7769d271cc41839cc5c1792679d4ac0408fa2bf
xzh <793414851@qq.com>
2017-05-10 15:23:15 +0800

    购物车效果基本完成

--- src/js/buylist.js
+++ src/js/buylist.js
@@ -32,12 +32,24 @@ requirejs(["config"],function(){
 				$(this).addClass('click');
 				$('.num .detail-c').html($(this).attr('title'));
 			});
-			
 			$('.size').on('click','span',function(){
 				$(this).siblings().removeClass('click');
 				$(this).addClass('click');
-				$('.num .detail-n').html($(this).html());
-				
+				$('.num .detail-n').html($(this).html());	
+			});
+			
+			//购物车效果
+			$("#btn-buycar").on('click',function(){
+				if($(".detail-c").html() !== '' && $(".detail-n").html()!==''){
+					$('#nor-img').append($('#nor-img img').clone().addClass('moveImg').css({'position':'absolute','z-index':100000,'top':0,'left':0}));
+					//top=购物车偏移高度-图片的偏移高度+滚动条滚动距离
+					$('.moveImg').stop(true).animate({'width':0,'left':$('.Bcar').offset().left,'top':$('.Bcar').position().top-180+window.scrollY},function(){
+						//动画效果完成后移除克隆的元素
+						$('.moveImg').remove();
+					});
+				}else{
+					alert('请选择您所购买的商品的颜色以及尺码！！');
+				}
 			});
 		});
 		
