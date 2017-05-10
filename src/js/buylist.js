35b06a7a6c28066ba697bc6de8bb3116fee18ac4
xzh <793414851@qq.com>
2017-05-10 17:52:44 +0800

    放大镜优化以及cookie的生成

--- src/js/buylist.js
+++ src/js/buylist.js
@@ -1,5 +1,5 @@
 requirejs(["config"],function(){
-	requirejs(["jquery","jqueryUi","jqueryFdj"],function($){
+	requirejs(["jquery","jqueryUi","jqueryFdj","jquerycookie"],function($){
 		//加载头尾部
 		$(function(){
 			$('#header').load('header.html');
@@ -37,16 +37,42 @@ requirejs(["config"],function(){
 				$(this).addClass('click');
 				$('.num .detail-n').html($(this).html());	
 			});
-			
+			console.log($('.price').html());
 			//购物车效果
 			$("#btn-buycar").on('click',function(){
 				if($(".detail-c").html() !== '' && $(".detail-n").html()!==''){
 					$('#nor-img').append($('#nor-img img').clone().addClass('moveImg').css({'position':'absolute','z-index':100000,'top':0,'left':0}));
 					//top=购物车偏移高度-图片的偏移高度+滚动条滚动距离
-					$('.moveImg').stop(true).animate({'width':0,'left':$('.Bcar').offset().left,'top':$('.Bcar').position().top-180+window.scrollY},function(){
+					$('.moveImg').stop(true).animate({'width':0,'left':$('.Bcar').offset().left-20,'top':$('.Bcar').position().top-180+window.scrollY},function(){
 						//动画效果完成后移除克隆的元素
 						$('.moveImg').remove();
 					});
+					//生成cookie
+					var $carlist = $.cookie('carlist');
+					$carlist = $carlist ? JSON.parse($carlist):[];
+					var hasCarList = false;
+					for(i=0;i<$carlist.length;i++){
+						//判断商品id、颜色、码数是否一致，不然重新创建新的cookie
+	   			   		if($carlist[i].goodId == $('#goodId').html() && $carlist[i].goodColor == $('.color .click').attr('title') && $carlist[i].goodSize == $('.size .click').html()){
+	   			   			hasCarList = true;
+	   			   			$carlist[i].quan++;
+	   			   			break;
+	   			   		}
+	   			   	}
+					if(!hasCarList){
+						var good={
+							goodId:$('#goodId').html(),
+							goodName:$('#goods-r h2').html(),
+							goodPrice:$('.price').text(),
+							goodColor:$('.color .click').attr('title'),
+							goodSize:$('.size .click').html(),
+							goodUrl:$('#nor-img img').attr('src'),
+							quan:1
+						}
+						$carlist.push(good);
+					}
+					$.cookie('carlist',JSON.stringify($carlist),{path:"/",expires:7});
+					console.log($.cookie('carlist'));
 				}else{
 					alert('请选择您所购买的商品的颜色以及尺码！！');
 				}
--- src/js/config.js
+++ src/js/config.js
@@ -2,10 +2,12 @@ requirejs.config({
 	paths:{
 		"jquery":"../lib/jquery-3.1.1",
 		"jqueryUi":"../lib/jquery-ui-1.12.1.custom/jquery-ui",
-		"jqueryFdj":"../lib/jq-zoom/fdzoom"
+		"jqueryFdj":"../lib/jq-zoom/fdzoom",
+		"jquerycookie":"../lib/jquery.cookie"
 	},
 	shim:{
 		"jqueryUi":["jquery"],
-		"jqueryFdj":["jquery"]
+		"jqueryFdj":["jquery"],
+		"jquerycookie":["jquery"]
 	}
 });
